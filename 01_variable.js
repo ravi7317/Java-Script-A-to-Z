289ecc49f4c04135eff37513e3b410b32d615733
Ravi Verma <+>
2024-05-20 18:47:39 +0530

    added js basic concepts

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+const accountId= 144553
+let accountEmail ="ravi@gmail.com"
+var accountPassword ="12443"
+accountCity ="Phagwara"
+
+//accountId =2
+/*
+preffer nit to use var 
+because of isuue in block spope and functional scope
+*/
+accountEmail="verma@gmail.com"
+accountPassword="124u80"
+accountCity="jaipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword, accountCity]);
---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treal all js code as a newer version
+let name = "ravi"
+let age = 18
+let isloggedIn = true
+let state = null
+// number =>2 to power 53
+// brgint
+// string => ""
+// boolean = true /fals 
+// null => standalone value 
+// undefined = means we are not assign any value 
+// symbol => unique
+ // these all are premitive data types 
+
+ //------------------//
+ //object 
+ console.log(typeof "ravi")
+ console.log(typeof 18)
+ console.log(typeof null)
