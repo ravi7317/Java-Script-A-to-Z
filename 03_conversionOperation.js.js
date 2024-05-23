1538efdc08e5b22d297c218df7fbe7531b031af4
Ravi Verma <+>
2024-05-23 17:22:37 +0530

    Added

---
+++ 03_conversionOperation.js.js
@@ -0,0 +1,12 @@
+let score ="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInnumber =Number(score)
+console.log(typeof valueInnumber)
+//"33" it will convert easly 
+// "33abc" => Nan not a number 
+// true => 1; false >=0  
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
