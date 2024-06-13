d5cb7ebb0c2225d277ad79fc79763b5789b0fea7
Ravi Verma <+>
2024-06-13 21:34:16 +0530

    Added

---
+++ 03_1_Operation.js
@@ -0,0 +1,13 @@
+let value = 3
+let negvalue= -value
+//console.log(negvalue)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+let str1="ravi";
+let str2=" verma";
+let str3=str1+str2;
+console.log(str3);
